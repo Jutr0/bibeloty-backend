87d5c3efd51e6b23dda87f041d3a50eaa45e4641
Victor Kielczewski <victor.kielczewski@gdsoft.info>
2024-08-08 22:15:12 +0200

    Created Table Modal and Button componetns + added css variables

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,17 @@ module ECommerce
   class Application < Rails::Application
     config.load_defaults 7.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 methods: [:get, :post, :put, :patch, :delete, :options, :head],
+                 expose: ['Authorization'],
+                 max_age: 600
+      end
+    end
+
     config.api_only = true
   end
 end
