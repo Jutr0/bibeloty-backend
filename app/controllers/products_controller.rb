ab59f80d3a92da9014f22a0597a72bce58bdafcf
wiktor <micwikski@gmail.com>
2025-01-09 19:10:50 +0100

    responsive design + bug fixes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
-    @products_sections = Product.joins(:section).reorder('sections.order, products.created_at desc').group_by(&:section)
+    @products_sections = Product.includes(:section).reorder('sections.order, products.created_at desc').group_by(&:section)
   end
 
   def show
